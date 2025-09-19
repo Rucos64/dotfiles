1416a227b1e7d6a186ad1ff8275a5c434770a012
Max <maksi.tihan71@gmail.com>
2025-09-19 21:21:41 +0300

    cleanup

--- firefox/betterfox.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-CONFIG=$(
-	cd ~/.mozilla/firefox/*default-release &&
-		pwd || exit
-)/user.js
-
-curl https://raw.githubusercontent.com/yokoffing/Betterfox/main/user.js >"$CONFIG" &&
-	cat ~/dotfiles/firefox/user.js >>"$CONFIG"
--- firefox/startpage.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo cp ~/dotfiles/firefox/autoconfig.js /usr/lib64/firefox/defaults/pref/
-sudo cp ~/dotfiles/firefox/autoconfig.cfg /usr/lib64/firefox/
