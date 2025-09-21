8b729f8dca8a9580c4ad5f6db0699f75535b17fd
Rucos64 <maksi.tihan71@gmail.com>
2025-09-22 00:42:02 +0300

    changed my wallpaper

--- darkman/.local/share/dark-mode.d/dark.sh
+++ darkman/.local/share/dark-mode.d/dark.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env dash
 
-runapp swaybg -i /home/igor/Pictures/wallpapers/dark.avif
+runapp swaybg -m fill -i /home/max/Pictures/wallpapers/dark.jpg
 [ $(pgrep -c swaybg) -gt 1 ] && pkill -o swaybg
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 
-kvantummanager --set ColloidDark
-sed -i 's/icon_theme=.*/icon_theme=Papirus-Dark/' ~/.config/qt6ct/qt6ct.conf
+# kvantummanager --set ColloidDark
+# sed -i 's/icon_theme=.*/icon_theme=Papirus-Dark/' ~/.config/qt6ct/qt6ct.conf
 export QT_QPA_PLATFORMTHEME=qt6ct
 
 ln -fs ~/dotfiles/rofi/.config/rofi/themes/dark.rasi ~/.config/rofi/theme.rasi
--- darkman/.local/share/light-mode.d/light.sh
+++ darkman/.local/share/light-mode.d/light.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env dash
 
-runapp swaybg -i /home/igor/Pictures/wallpapers/light.avif
+runapp swaybg -m fill -i /home/max/Pictures/wallpapers/light.jpg
 [ $(pgrep -c swaybg) -gt 1 ] && pkill -o swaybg
 
 gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
 gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Light'
 
-kvantummanager --set Colloid
-sed -i 's/icon_theme=.*/icon_theme=Papirus/' ~/.config/qt6ct/qt6ct.conf
+# kvantummanager --set Colloid
+# sed -i 's/icon_theme=.*/icon_theme=Papirus/' ~/.config/qt6ct/qt6ct.conf
 export QT_QPA_PLATFORMTHEME=qt6ct
 
 ln -fs ~/dotfiles/rofi/.config/rofi/themes/light.rasi ~/.config/rofi/theme.rasi
